7658778a1ddc1683060e206d0f4d8ac110ec10ac
JACK DUB DUB <jackduboulay98@gmail.com>
2026-03-17 17:58:52 +0800

    Lab1 dir created
    Forgot to add Lab1's exercise.

---
+++ Lab1/helloworld.c
@@ -0,0 +1,12 @@
+// Jack du Boulay - 32712899
+// ICT159 - Foundations of Programming -> Lab 1 
+
+// Libraries
+#include <stdio.h> 
+
+// Main
+int main()
+{
+	printf("Hello world!\n"); // print message then go to next line
+	return(0);  // exit program with no error
+}
