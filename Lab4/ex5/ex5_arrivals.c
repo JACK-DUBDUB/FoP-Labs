339e9a9645e082935e24a7dcecc5c531766aafb0
JACK DUB DUB <jackduboulay98@gmail.com>
2026-03-19 01:50:49 +0800

    Still stuck on exercise 5
    Its a lot more difficult than i expected.

--- Lab4/ex5/ex5_arrivals.c
+++ Lab4/ex5/ex5_arrivals.c
@@ -11,7 +11,6 @@
 
 // Declared functions
 int getDepTime();
-int validator(int input[]);
 double getDist();
 double getSpeed();
 
@@ -23,7 +22,8 @@ int main()
         printf("\nNot a valid time\n");
         return 1;
     }
-       
+    
+    printf("24H time: %d", departureT);
 
     //double distance = getDist();
     //double speed = getSpeed();
@@ -35,45 +35,59 @@ int main()
 
 int getDepTime()
 {
-    int inputDepTime;
+    int inputHours, inputMinutes;
+    int notation = 0;
+    char *clockDis[] = {"AM", "PM"};
     printf("Enter departure time (24hr): ");
 
+    /** What a shit show this part was.
+     * Most difficult exercise as of yet.
+     * Unless there was an alternative way of going about this, but i'm not knowledgable enough about C
+     * 
+     * /// How to get values for two different variables in a single scanf /// -> https://stackoverflow.com/a/18832043 
+     * My old way was too cluttered before lord knows how everyone else did this exercise
+     * 
+     * /// How to get only two digits using scanf /// -> https://stackoverflow.com/a/31630375
+     * Tricky, i didn't know it was a thing, but it is!
+     * %2d <- takes two digits only.
+     * However, it does not reject numbers larger than 2 digits, which means the input buffer may have chars remaining still.
+     * 
+     * Was also momentarily stuck on the if(scanf()) part but this answer clarified it ->  https://stackoverflow.com/a/16685393
+     * int result = ("%2d %2d ", &x, &y); returns a value of 2 if it worked
+     * 
+     * /// Other neat tricks
+     * scanf("%d ", &x);    This actually removes any trailing white spaces 
+     * scanf("%d*c", &x);   This actually performs a getchar() but its kind of fkn useless if theres many values still residing in buffer  
+     * scanf("%11s", name) where limit = 12, it reads a maxmimum of 11 + the null terminator
+     * scanf("%[abcde]", letters); This is a scan set -> any letter than isn't contained in the brackets is not read (a rinky dink REGEX)
+     */
+
     // Get Input and check if it was valid.
-    if (scanf("%d", &inputDepTime) != 1)
+    if (scanf("%2d %2d ", &inputHours, &inputMinutes) != 2)   // We are inputting values into two variables so... != 2 :^)  silly billy 
         return -1;
     if (getchar() != '\n')
         return -1;
 
-    // Clock notation: AM = 0, PM = 1
-    int notation = 0;
-    if (inputDepTime / 100 > 11)
+    // Change to 12 hour time
+    if((inputHours - 12) >= 0)
     {
-         inputDepTime -= 1200;                  // Half it obviously... make it easier for yourself, jesus
-         notation = 1;
+        inputHours -= 12;   // Removing the 24hr time
+        notation = 1;       // PM
     }
-       
-    // Get each individual digit
-    int inheritVal = inputDepTime;
-    int validateTime[LIMIT] = {0, 0, 0, 0};     // This will flag if the value's inserted are invalid -> if values were inserted at all?
-    int divisor = 1000;                         // This is okay because we do not go past 1
-    for (int i = 0; i < LIMIT; i++)
-    {
-        validateTime[i] = inheritVal / divisor;
-        inheritVal = inheritVal % divisor;
-        divisor = divisor / 10;
-        //printf("%d  ", validateTime[i]);
-    }
+    
+    // Validate
+    if(inputHours < 0 || inputHours > 11 || inputMinutes < 0 || inputMinutes > 59)
+        return-1;
 
-    // Validate the number
-    int isValid = validator(validateTime);
-    if (!isValid)
-        return -1;
+    // Display 12 hour time
+    printf("The departure time is: %d%d:%d%d %s\n", (inputHours / 10), (inputHours % 10), (inputMinutes / 10), (inputHours % 10), clockDis[notation]);
 
-    // Convert to 12hr time
-    char *halves[] = {"AM", "PM"};
-    printf("\nThe departure time is %d%d:%d%d %s\n", validateTime[0], validateTime[1], validateTime[2], validateTime[3], halves[notation]);
+    // Revert back to 24 hour time
+    if(notation)
+        inputHours += 12;
 
-    return inputDepTime;
+    // Can only return a single number
+    return (inputHours * 100) + inputMinutes;
 }
 
 double getDist()
@@ -90,22 +104,4 @@ double getSpeed()
     double speed;
     printf("Speed (km/h): ");
     scanf("%f", &speed);
-}
-
-
-int validator(int input[])
-{
-    for (int i = 0; i < LIMIT; i++)
-    {
-        int digit = input[i];
-        if(digit < 0 || digit > 9)                      // Any digit
-            return 0;
-        if (i == 0 && digit > 1)                        // digit 1
-            return 0;
-        if (i == 1 && input[0] == 1 && digit > 1)       // digit 2
-            return 0;
-        if (i == 2 && digit > 5)                        // digit 3
-            return 0;
-    }
-    return 1;
 }
