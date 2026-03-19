fee36b326e424cb962f29e92e2f25dd074eb31af
JACK DUB DUB <jackduboulay98@gmail.com>
2026-03-19 14:18:35 +0800

    Brief commit
    Ground work planned out for how I could simplify my code further.
    -> Use one function for getting the time for bother departure and arrivals

--- Lab4/ex5/ex5_arrivals.c
+++ Lab4/ex5/ex5_arrivals.c
@@ -10,22 +10,42 @@
 #define LIMIT 4
 
 // Declared functions
-int getDepTime();
+int getTime(int compare);
 double getDist();
 double getSpeed();
 
 int main()
 {
-    int departureT = getDepTime();
+    int arrivalT = 0;
+
+    /// DEPARTURE TIME ///
+    printf("Enter departure time (24hr): ");
+    int departureT = getTime(arrivalT);
     if (departureT == -1)
     {
-        printf("\nNot a valid time\n");
+        printf("\nNot a valid departure time\n");
         return 1;
     }
-    
-    printf("24H time: %d", departureT);
+    printf("24H time: %d\n", departureT);
 
-    //double distance = getDist();
+    /// DISTANCE ///
+    double destinationdist = getDist();
+    if (destinationdist == -1)
+    {
+        printf("\nNot a valid distance\n");
+        return 2;
+    }
+    printf("Distance selected: %f km\n", destinationdist);
+    
+    /// ARRIVAL TIME ///
+    arrivalT = getTime(departureT);
+    printf("Enter arrival time (24hr): ");
+    if (arrivalT == -1)
+        {
+        printf("\nNot a valid arrival time\n");
+        return 3;
+    }   
+
     //double speed = getSpeed();
 
     //double arrivalT = distance / speed;
@@ -33,19 +53,18 @@ int main()
     return 0;
 }
 
-int getDepTime()
+int getTime(int compare)
 {
-    int inputHours, inputMinutes;
+    int inputHours, inputMinutes = {0, 0};
     int notation = 0;
     char *clockDis[] = {"AM", "PM"};
-    printf("Enter departure time (24hr): ");
 
     /** What a shit show this part was.
      * Most difficult exercise as of yet.
      * Unless there was an alternative way of going about this, but i'm not knowledgable enough about C
      * 
      * /// How to get values for two different variables in a single scanf /// -> https://stackoverflow.com/a/18832043 
-     * My old way was too cluttered before lord knows how everyone else did this exercise
+     * My old getDepTime() was too complicated, but it worked, it was just really uneccessary as seen here -> https://github.com/JACK-DUBDUB/FoP-Labs/blob/1faded46f61b7fbbed663ef9187e635c659f349d/Lab4/ex5/ex5_arrivals.c
      * 
      * /// How to get only two digits using scanf /// -> https://stackoverflow.com/a/31630375
      * Tricky, i didn't know it was a thing, but it is!
@@ -63,7 +82,7 @@ int getDepTime()
      */
 
     // Get Input and check if it was valid.
-    if (scanf("%2d %2d ", &inputHours, &inputMinutes) != 2)   // We are inputting values into two variables so... != 2 :^)  silly billy 
+    if (scanf("%2d %2d", &inputHours, &inputMinutes) != 2)   // We are inputting values into two variables so... != 2 :^)  silly billy 
         return -1;
     if (getchar() != '\n')
         return -1;
@@ -74,28 +93,34 @@ int getDepTime()
         inputHours -= 12;   // Removing the 24hr time
         notation = 1;       // PM
     }
-    
-    // Validate
+
+    // Validate that is within 12 hour clock structure
     if(inputHours < 0 || inputHours > 11 || inputMinutes < 0 || inputMinutes > 59)
-        return-1;
+        return -1;
 
-    // Display 12 hour time
-    printf("The departure time is: %d%d:%d%d %s\n", (inputHours / 10), (inputHours % 10), (inputMinutes / 10), (inputHours % 10), clockDis[notation]);
-
-    // Revert back to 24 hour time
-    if(notation)
-        inputHours += 12;
+    int time24h = ((inputHours + 12) * 100) + inputMinutes;
+    
+    // Display 12 hour time - I think seeing a leading 0 looks nicer to me.
+    if(compare <= time24h)
+        printf("The departure time is: %d%d:%d%d %s\n", (inputHours / 10), (inputHours % 10), (inputMinutes / 10), (inputMinutes % 10), clockDis[notation]);
+    else if (compare > time24h)
+        printf("The arrival time is: %d%d:%d%d %s\n", (inputHours / 10), (inputHours % 10), (inputMinutes / 10), (inputMinutes % 10), clockDis[notation]);
 
     // Can only return a single number
-    return (inputHours * 100) + inputMinutes;
+    return time24h;
 }
 
 double getDist()
 {
     double dist;
     printf("Distance (km): ");
-    scanf("%f", &dist);
-
+    if(scanf("%f", &dist) != 1)
+        return -1;
+    if(getchar() != '\n')
+        return -1;
+    if(dist < 0)
+        return -1;
+    
     return dist;
 }
 
@@ -104,4 +129,6 @@ double getSpeed()
     double speed;
     printf("Speed (km/h): ");
     scanf("%f", &speed);
+
+    return speed;
 }
