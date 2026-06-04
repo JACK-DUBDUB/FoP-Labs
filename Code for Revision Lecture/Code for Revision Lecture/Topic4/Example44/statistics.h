6e0f90caf7323e9741d751d077aa2dbcb7101a7f
JACK DUB DUB <jackduboulay98@gmail.com>
2026-06-04 14:48:32 +0800

    Tidied up

--- Code for Revision Lecture/Code for Revision Lecture/Topic4/Example44/statistics.h
+++
@@ -1,9 +0,0 @@
-#ifndef statistics_h
-#define statistics_h
-
-float ComputeDiffMaxMean(float A, float B, float C);
-float ComputeMax(float A, float B, float C);
-float ComputeMean(float A, float B, float C);
-float ComputeDiff(float A, float B);
-
-#endif /* statistics_h */
